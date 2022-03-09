e3adfbaf58cfa962211947ac7d1fd13316ede176
AMastini <amastini@webeasytech.com>
2022-03-09 14:05:23 +0100

    Improved the way the whole module works

--- na_stock_picking/models/stock_picking.py
+++ na_stock_picking/models/stock_picking.py
@@ -8,16 +8,3 @@ class StockPicking(models.Model):
 
     ddt_num = fields.Char(string='Numero DDT')
     delivery_date = fields.Date(string="Data consegna")
-    picking_type_value = fields.Char(compute="check_picking_type", stored=1)
-
-    @api.depends('picking_type_value')
-    def check_picking_type(self):
-        stock = self.env['stock.picking'].browse(self.id)
-        if stock.id > 0:
-            for record in self:
-                if stock.picking_type_code == 'incoming':
-                    record.picking_type_value = 'incoming'
-                elif stock.picking_type_code == 'outgoing':
-                    record.picking_type_value = 'outgoing'
-        else:
-            raise UserError('L\'ordine non esiste.')
