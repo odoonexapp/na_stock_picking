5aaec045b5e6d5a6446a9d04b9604906f028ed48
AMastini <amastini@webeasytech.com>
2022-03-09 14:11:34 +0100

    Changed the fields name so that the module is following the company guidelines

--- na_stock_picking/models/stock_picking.py
+++ na_stock_picking/models/stock_picking.py
@@ -6,5 +6,5 @@ from odoo.exceptions import UserError
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    ddt_num = fields.Char(string='Numero DDT')
-    delivery_date = fields.Date(string="Data consegna")
+    ddt_supplier_number = fields.Char(string='Numero DDT')
+    ddt_supplier_date = fields.Date(string="Data consegna")
