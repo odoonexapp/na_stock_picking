2cef3f9117bec868597a00e93fe285650a7a39ea
AMastini <amastini@webeasytech.com>
2022-03-10 17:48:55 +0100

    [FIX] Module name change

--- na_supplier_ddt_number/__manifest__.py
+++ na_supplier_ddt_number/__manifest__.py
@@ -1,7 +1,7 @@
 {
-    'name': 'Nexapp Stock picking',
-    'summary': 'Stock picking',
-    'description': """Stock picking""",
+    'name': 'Nexapp Supplier DDT Number',
+    'summary': 'Added the supplier DDT Number and date',
+    'description': """Added the supplier DDT Number and date""",
     'version': '14.0.1.1',
     'images': ['images/stock_picking_screenshot.png'],
     'depends': [
